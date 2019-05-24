5e5c8dfa5e0b5ac43dfb28ef8a78d666ed666866
Benjamin Dordoigne <benjamin.d@onthegosystems.com>
2019-05-24 07:37:12 +0100

    Project Cont

--- inc/widgets.php
+++ inc/widgets.php
@@ -192,6 +192,31 @@ if ( ! function_exists( 'understrap_widgets_init' ) ) {
 			)
 		);
 
+		register_sidebar(
+			array(
+				'name'          => __( 'Copyright Left', 'understrap' ),
+				'id'            => 'copyleft',
+				'description'   => __( 'Full sized footer widget with dynamic grid', 'understrap' ),
+				'before_widget' => '<div id="%1$s" class="footer-widget %2$s dynamic-classes">',
+				'after_widget'  => '</div><!-- .footer-widget -->',
+				'before_title'  => '<h3 class="widget-title">',
+				'after_title'   => '</h3>',
+			)
+		);
+
+		register_sidebar(
+			array(
+				'name'          => __( 'WebsiteBy Right', 'understrap' ),
+				'id'            => 'websiteright',
+				'description'   => __( 'Full sized footer widget with dynamic grid', 'understrap' ),
+				'before_widget' => '<div id="%1$s" class="footer-widget %2$s dynamic-classes">',
+				'after_widget'  => '</div><!-- .footer-widget -->',
+				'before_title'  => '<h3 class="widget-title">',
+				'after_title'   => '</h3>',
+			)
+		);
+
+
 
 	}
 } // endif function_exists( 'understrap_widgets_init' ).
