c887ace0dbb3299b4a280919302ee4216e0bf754
Benjamin Dordoigne <benjamin.d@onthegosystems.com>
2019-06-14 16:19:56 +0100

    Updated files

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,9 @@
+//document.querySelector('.entry-content h2').textContent = 'Hello World';
+
+document.querySelector('.page-id-434 .entry-content h2').style.display = "none";
+
+document.querySelector('.page-id-434 .entry-content p').textContent = "Submit the form below to become a coach.";
 
 
-document.querySelector('.search-section h1').textContent = 'Test';
 
 
-console.log('Test ');
---
+++ single-coaches.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * The main template file.
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package understrap
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+get_header();
+
+$container = get_theme_mod( 'understrap_container_type' );
+?>
+
+
+<div id="profile-bg" style="background-image: url(<?php the_field('header_background'); ?>);">
+	<div class="profile-info"> 
+
+		<h1><?php the_field('name'); ?></h1>
+		
+
+			<?php 
+			
+				$rating = get_field('start_rating');
+
+				for ($i = 0; $i < $rating; $i++) { ?>
+					<i class="fas fa-star"></i>
+				<?php }
+			
+			?>
+	</div>
+</div>
+
+
+
+
+<div class="wrapper" id="index-wrapper">
+
+	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
+
+		<div class="row">
+		<main class="site-main" id="main">
+
+			<h2><?php the_field('name'); ?></h2>
+
+			<h2><?php the_field('start_rating'); ?></h2>
+
+			<?php 
+			
+				$rating = get_field('start_rating');
+
+				for ($i = 0; $i < $rating; $i++) { ?>
+					<i class="fas fa-star"></i>
+				<?php }
+			
+			?>
+
+			<h2><?php the_field('coach_number'); ?></h2>
+
+			
+
+			<?php the_field('coach_text'); ?>
+			<div class="coach-profile-products">
+				<?php the_field('coach_products'); ?>
+			</div>
+
+			
+
+
+
+
+			
+
+				
+
+			</main><!-- #main -->
+
+			
+
+			
+
+		</div><!-- .row -->
+
+	</div><!-- #content -->
+
+</div><!-- #index-wrapper -->
+
+<?php get_footer(); ?>
